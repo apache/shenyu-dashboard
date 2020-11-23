ce898f927eaa2ec9af0d3ef342b0ee7af467bea6
ChiChi7777777 <18375337360@163.com>
2020-11-24 01:05:26 +0800

    Make rate_limiter using common plugin handle (#25)

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -155,7 +155,7 @@ class BasicLayout extends React.PureComponent {
   render() {
     const { collapsed, routerData, match, location, plugins, dispatch, } = this.props;
     const bashRedirect = this.getBaseRedirect();
-    const systemRoute = ["rewrite", "rate_limiter", "divide", "springCloud", "hystrix"];
+    const systemRoute = ["rewrite", "divide", "springCloud", "hystrix"];
     let menus = getMenuData();
     plugins.forEach((item) => {
       if (systemRoute.indexOf(item.name) === -1) {
