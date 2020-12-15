76e4ddd4963c3eef67a06a5363f9c729c09ac919
dengliming <liming.d.pro@gmail.com>
2020-12-15 23:13:04 +0800

    Optimize internationalization of dynamic menus (#49)

--- src/common/menu.js
+++ src/common/menu.js
@@ -3,50 +3,60 @@ import { getIntlContent } from '../utils/IntlUtils'
 /** 菜单 移到 统一地方 处理 */
 export const menuData = [
   {
-    name: getIntlContent("SOUL.MENU.PLUGIN.LIST"),
+    name: getIntlContent('SOUL.MENU.PLUGIN.LIST'),
     icon: 'dashboard',
     path: 'plug',
+    locale: 'SOUL.MENU.PLUGIN.LIST',
     children: [
       {
         name: 'divide',
         path: 'divide',
-        id: 'divide6'
+        id: 'divide6',
+        locale: 'SOUL.MENU.PLUGIN.DIVIDE'
       },
       {
         name: 'hystrix',
         path: 'hystrix',
-        id: 'hystrix9'
+        id: 'hystrix9',
+        locale: 'SOUL.MENU.PLUGIN.HYSTRIX'
       }
     ],
   },
   {
-    name: getIntlContent("SOUL.MENU.SYSTEM.MANAGMENT"),
+    name: getIntlContent('SOUL.MENU.SYSTEM.MANAGMENT'),
     icon: 'setting',
     path: 'system',
+    locale: 'SOUL.MENU.SYSTEM.MANAGMENT',
     children: [
       {
-        name: getIntlContent("SOUL.MENU.SYSTEM.MANAGMENT.USER"),
+        name: getIntlContent('SOUL.MENU.SYSTEM.MANAGMENT.USER'),
         path: 'manage',
+        locale: 'SOUL.MENU.SYSTEM.MANAGMENT.USER'
       },
       {
-        name: getIntlContent("SOUL.MENU.SYSTEM.MANAGMENT.PLUGIN"),
+        name: getIntlContent('SOUL.MENU.SYSTEM.MANAGMENT.PLUGIN'),
         path: 'plugin',
+        locale: 'SOUL.MENU.SYSTEM.MANAGMENT.PLUGIN'
       },
       {
-        name: getIntlContent("SOUL.PLUGIN.PLUGINHANDLE"),
+        name: getIntlContent('SOUL.PLUGIN.PLUGINHANDLE'),
         path: 'pluginhandle',
+        locale: 'SOUL.PLUGIN.PLUGINHANDLE'
       },
       {
-        name: getIntlContent("SOUL.MENU.SYSTEM.MANAGMENT.AUTHEN"),
-        path: 'auth'
+        name: getIntlContent('SOUL.MENU.SYSTEM.MANAGMENT.AUTHEN'),
+        path: 'auth',
+        locale: 'SOUL.MENU.SYSTEM.MANAGMENT.AUTHEN'
       },
       {
-        name: getIntlContent("SOUL.MENU.SYSTEM.MANAGMENT.METADATA"),
-        path: 'metadata'
+        name: getIntlContent('SOUL.MENU.SYSTEM.MANAGMENT.METADATA'),
+        path: 'metadata',
+        locale: 'SOUL.MENU.SYSTEM.MANAGMENT.METADATA'
       },
       {
-        name: getIntlContent("SOUL.MENU.SYSTEM.MANAGMENT.DICTIONARY"),
-        path: 'dict'
+        name: getIntlContent('SOUL.MENU.SYSTEM.MANAGMENT.DICTIONARY'),
+        path: 'dict',
+        locale: 'SOUL.MENU.SYSTEM.MANAGMENT.DICTIONARY'
       }
 
     ],
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -159,7 +159,7 @@ class BasicLayout extends React.PureComponent {
     let menus = getMenuData();
     plugins.forEach((item) => {
       if (systemRoute.indexOf(item.name) === -1) {
-        menus[0].children.push({ name: item.name, path: `/plug/${item.name}`, authority: undefined, id: item.id })
+        menus[0].children.push({ name: item.name, path: `/plug/${item.name}`, authority: undefined, id: item.id, locale: (`SOUL.MENU.PLUGIN.${ item.name.toUpperCase()}`) })
       }
     })
     const layout = (
