28b847569341e72d1f4eb6c8f048f81ecf69d5a8
SaberSola <1198902364@qq.com>
2021-06-19 23:20:28 +0800

    [ISSUE #126 ]support param-mapping plugin (#128)
    * add param mapping plugin for shenyu
    
    * add param mapping plugin for shenyu
    
    Co-authored-by: zhanglei <zhanglei06469@hellobike.com>

--- src/routes/Plugin/PluginRuleHandle/index.js
+++ src/routes/Plugin/PluginRuleHandle/index.js
@@ -1,8 +1,9 @@
 import RequestRuleHandle from './RequestRuleHandle';
 import HystrixRuleHandle from './HystrixRuleHandle';
+import ParamPluginRuleHandle from "./ParamPluginRuleHandle";
 
 export default {
     request:RequestRuleHandle,
     hystrix:HystrixRuleHandle,
+    param_mapping: ParamPluginRuleHandle
 }
-
