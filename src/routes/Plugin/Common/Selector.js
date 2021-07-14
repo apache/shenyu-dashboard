f8959f10ffa099676e2ea7b8b8d5dbc1dbae4c2a
Zhang Yonglun <zhangyonglun@apache.org>
2021-07-14 17:47:40 +0800

    add license headers (#135)

--- src/routes/Plugin/Common/Selector.js
+++ src/routes/Plugin/Common/Selector.js
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { Component, Fragment } from "react";
 import {Modal, Form, Select, Input, Switch, Button, message, Tooltip, Popconfirm} from "antd";
 import { connect } from "dva";
@@ -20,7 +37,7 @@ class AddModal extends Component {
     this.state = {
       selectValue
     };
-    
+
     this.initSelectorCondtion(props);
     this.initDics();
   }
@@ -303,8 +320,8 @@ class AddModal extends Component {
                   <Select placeholder={getIntlContent("SHENYU.COMMON.MATCHTYPE")}>
                     {matchModeDics&&matchModeDics.map(item => {
                       return (
-                        <Option 
-                          key={item.dictValue} 
+                        <Option
+                          key={item.dictValue}
                           value={item.dictValue}
                         >
                           {item.dictName}
--- src/utils/AuthButton.js
+++ src/utils/AuthButton.js
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { Component } from 'react';
 import { Button, Popconfirm } from 'antd';
 import PropTypes from 'prop-types'
@@ -8,7 +25,7 @@ let buttonCache = {};
 
 /**
  *  reset authorized button cache
- * 
+ *
  */
 export function resetAuthButtonCache() {
   buttonCache = {};
@@ -16,7 +33,7 @@ export function resetAuthButtonCache() {
 
 /**
  * check button's authority
- * 
+ *
  * @param {string} perms
  * @param {Array} permissions
  */
@@ -28,11 +45,11 @@ export function checkButtonAuth(perms, permissions) {
     let {button : functionsList} = permissions;
     let authFunctions = functionsList.filter((item) => {
       return item.perms === perms
-    }) 
+    })
     const authFunction = (authFunctions && authFunctions.length > 0) ? authFunctions[0] : null;
     if(authFunction){
       buttonCache.perms = authFunction;
-    } 
+    }
     return authFunction;
   } else {
     return false;
