a03ac3b3bac5bbe0ae7484b84559b9d1d272618d
WeiS <78211515+QinZiWeiS@users.noreply.github.com>
2022-10-05 13:16:47 +0800

    Fix verification code bug (#245)
    * fix Verfication code bug

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -36,6 +36,10 @@ export default class LoginPage extends Component {
     this.ChildRef = React.createRef();
   }
 
+  componentDidMount() {
+    this.ChildRef.current.handleChange();
+  }
+
   handleSubmit = (err, values) => {
 
     const { dispatch } = this.props;
@@ -67,6 +71,8 @@ export default class LoginPage extends Component {
   codeError = () => {
     return this.state.codeError ? <span /> : <span className={styles.codeError} id='codeError'>Please enter correct verify code!</span>
   }
+
+
 
   render() {
     const { submitting } = this.props;
