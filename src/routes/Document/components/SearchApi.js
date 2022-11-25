8f379a923e880354802ed351cce90dc2783e2008
wenyu.dai <wenyu.dai@qq.com>
2022-11-25 17:58:50 +0800

    change api

--- src/routes/Document/components/SearchApi.js
+++ src/routes/Document/components/SearchApi.js
@@ -1,128 +1,141 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+	* Licensed to the Apache Software Foundation (ASF) under one or more
+	* contributor license agreements. See the NOTICE file distributed with
+	* this work for additional information regarding copyright ownership.
+	* The ASF licenses this file to You under the Apache License, Version 2.0
+	* (the "License"); you may not use this file except in compliance with
+	* the License. You may obtain a copy of the License at
+	*
+	* http://www.apache.org/licenses/LICENSE-2.0
+	*
+	* Unless required by applicable law or agreed to in writing, software
+	* distributed under the License is distributed on an "AS IS" BASIS,
+	* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	* See the License for the specific language governing permissions and
+	* limitations under the License.
+	*/
 
-import { Tree, Input, Empty } from "antd";
-import React, { useContext, useEffect, useState } from "react";
-import ApiContext from "./ApiContext";
-import { getIntlContent } from "../../../utils/IntlUtils";
+import { Tree, Empty, message, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+// import ApiContext from "./ApiContext";
+// import { getIntlContent } from "../../../utils/IntlUtils";
+import { getRootTag, getParentTagId, getApi } from "../../../services/api";
+import { Method } from "./globalData";
 
+const { Text } = Typography;
 const { TreeNode } = Tree;
-const { Search } = Input;
+// const { Search } = Input;
 
 function SearchApi(props) {
   const { onSelect } = props;
-  const [expandedKeys, setExpandedKeys] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [autoExpandParent, setAutoExpandParent] = useState(true);
-  const {
-    apiData: { menuProjects }
-  } = useContext(ApiContext);
+  // const [searchValue, setSearchValue] = useState("");
 
-  const renderTreeNode = data => {
+  // const handleSearchChange = e => {
+  //   const { value } = e.target;
+  //   const keys = [];
+  //   const findSearchKeys = data =>
+  //     data.forEach(item => {
+  //       if (item.label.indexOf(value) > -1 || item.name?.indexOf(value) > -1) {
+  //         keys.push(item.key);
+  //       }
+  //       if (Array.isArray(item.children)) {
+  //         findSearchKeys(item.children);
+  //       }
+  //     });
+  //   setSearchValue(value);
+  // };
+
+  const [apiTree, setApiTree] = useState([]);
+
+  const renderTreeNodes = data => {
     return data.map(item => {
-      const { children, id, label, key, name } = item;
-      const index = label.indexOf(searchValue);
-      const sameName = name?.indexOf(searchValue);
-      const beforeStr = label.substr(0, index);
-      const afterStr = label.substr(index + searchValue.length);
-      let titleObj = <span>{label}</span>;
-      if (index > -1) {
-        titleObj = (
-          <span>
-            {beforeStr}
-            <span style={{ color: "#f50" }}>{searchValue}</span>
-            {afterStr}
-          </span>
+      if (item.children) {
+        return (
+          <TreeNode
+            title={item.title}
+            key={item.key}
+            dataRef={item}
+            selectable={item.isLeaf}
+            isLeaf={item.isLeaf}
+          >
+            {renderTreeNodes(item.children)}
+          </TreeNode>
         );
       }
-      if (searchValue && sameName > -1) {
-        titleObj = <span style={{ color: "#f50" }}>{label}</span>;
-      }
-      return (
-        <TreeNode
-          key={key}
-          title={titleObj}
-          selectable={id !== undefined}
-          id={id}
-        >
-          {children?.length && renderTreeNode(children)}
-        </TreeNode>
-      );
+      return <TreeNode key={item.key} {...item} dataRef={item} />;
     });
   };
 
-  const handleSearchChange = e => {
-    const { value } = e.target;
-    const keys = [];
-    const findSearchKeys = data =>
-      data.forEach(item => {
-        if (item.label.indexOf(value) > -1 || item.name?.indexOf(value) > -1) {
-          keys.push(item.key);
-        }
-        if (Array.isArray(item.children)) {
-          findSearchKeys(item.children);
-        }
-      });
-    findSearchKeys(menuProjects);
-    setExpandedKeys(keys);
-    setSearchValue(value);
-    setAutoExpandParent(true);
+  const queryRootTag = async () => {
+    const { code, data = [], message: msg } = await getRootTag();
+    if (code !== 200) {
+      message.error(msg);
+      return;
+    }
+    setApiTree(
+      data?.map((item, index) => ({
+        ...item,
+        title: item.name,
+        key: index.toString(),
+        isLeaf: !item.hasChildren
+      })) || []
+    );
   };
 
-  const handleExpandChange = keys => {
-    setExpandedKeys(keys);
-    setAutoExpandParent(false);
-  };
-
-  useEffect(
-    () => {
-      if (Array.isArray(menuProjects)) {
-        const allKeys = [];
-        const getAllParentsKey = data =>
-          data.forEach(item => {
-            if (item.children) {
-              allKeys.push(item.key);
-              getAllParentsKey(item.children);
-            }
-          });
-        getAllParentsKey(menuProjects);
-        setExpandedKeys(allKeys);
+  const onLoadData = async treeNode => {
+    if (treeNode.props.children) {
+      return Promise.resolve();
+    }
+    const { id, hasChildren } = treeNode.props.dataRef;
+    if (hasChildren) {
+      const { code, message: msg, data } = await getParentTagId(id);
+      if (code !== 200) {
+        message.error(msg);
+        return Promise.reject();
       }
-    },
-    [menuProjects]
-  );
+      treeNode.props.dataRef.children = data?.map((item, index) => ({
+        ...item,
+        title: item.name,
+        key: `${treeNode.props.eventKey}-${index}`
+      }));
+    } else {
+      const { code, message: msg, data } = await getApi(id);
+      if (code !== 200) {
+        message.error(msg);
+        return Promise.reject();
+      }
+      const { dataList } = data;
+      treeNode.props.dataRef.children = dataList?.map((item, index) => ({
+        ...item,
+        title: (
+          <>
+            <Text code>{Method[item.httpMethod]}</Text> {item.apiPath}
+          </>
+        ),
+        key: `${treeNode.props.eventKey}-${index}`,
+        isLeaf: true
+      }));
+    }
+    setApiTree([...apiTree]);
+    return Promise.resolve();
+  };
+
+  useEffect(() => {
+    queryRootTag();
+  }, []);
 
   return (
     <div style={{ overflow: "auto" }}>
-      <Search
+      {/* <Search
         allowClear
         onChange={handleSearchChange}
         placeholder={getIntlContent(
           "SHENYU.DOCUMENT.APIDOC.SEARCH.PLACEHOLDER"
         )}
-      />
-      {menuProjects?.length ? (
-        <Tree
-          autoExpandParent={autoExpandParent}
-          expandedKeys={expandedKeys}
-          onExpand={handleExpandChange}
-          onSelect={onSelect}
-        >
-          {renderTreeNode(menuProjects)}
+      /> */}
+      {apiTree?.length ? (
+        <Tree loadData={onLoadData} onSelect={onSelect}>
+          {renderTreeNodes(apiTree)}
         </Tree>
       ) : (
         <Empty style={{ padding: "80px 0" }} description={false} />
