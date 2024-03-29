7a1473df80bf11c1f8cef628c167cc694866106c
Kerwin Bryant <kerwin612@qq.com>
2024-03-29 11:17:36 +0800

    Enhanced AuthButton component (#431)
    * Enhanced AuthButton component
    
    * format code style

--- src/utils/AuthButton.js
+++ src/utils/AuthButton.js
@@ -15,10 +15,10 @@
  * limitations under the License.
  */
 
-import React, { Component } from 'react';
-import { Button, Popconfirm } from 'antd';
-import PropTypes from 'prop-types'
-import { connect } from 'dva';
+import React, { Component } from "react";
+import { Button, Popconfirm } from "antd";
+import PropTypes from "prop-types";
+import { connect } from "dva";
 
 // button cache
 let buttonCache = {};
@@ -38,16 +38,22 @@ export function resetAuthButtonCache() {
  * @param {Array} permissions
  */
 export function checkButtonAuth(perms, permissions) {
-  if (perms && permissions && permissions.button && permissions.button.length > 0) {
-    if(buttonCache && buttonCache[perms]){
+  if (
+    perms &&
+    permissions &&
+    permissions.button &&
+    permissions.button.length > 0
+  ) {
+    if (buttonCache && buttonCache[perms]) {
       return buttonCache[perms];
     }
-    let {button : functionsList} = permissions;
+    let { button: functionsList } = permissions;
     let authFunctions = functionsList.filter((item) => {
-      return item.perms === perms
-    })
-    const authFunction = (authFunctions && authFunctions.length > 0) ? authFunctions[0] : null;
-    if(authFunction){
+      return item.perms === perms;
+    });
+    const authFunction =
+      authFunctions && authFunctions.length > 0 ? authFunctions[0] : null;
+    if (authFunction) {
       buttonCache.perms = authFunction;
     }
     return authFunction;
@@ -57,42 +63,59 @@ export function checkButtonAuth(perms, permissions) {
 }
 
 @connect(({ global }) => ({
-    global,
-  }))
+  global,
+}))
 export default class AuthButton extends Component {
-
   constructor(props) {
     super(props);
-    const { global: { permissions } } = props;
+    const {
+      global: { permissions },
+    } = props;
     if (!permissions || !permissions.menu || permissions.menu.length === 0) {
       resetAuthButtonCache();
     }
   }
 
   render() {
-    const {perms, children, global: {permissions} } = this.props;
+    const {
+      perms,
+      noAuth,
+      children,
+      global: { permissions },
+    } = this.props;
     const authButton = checkButtonAuth(perms, permissions);
-    if(authButton){
-      if(authButton.icon){
+    if (authButton) {
+      if (authButton.icon) {
         const type = children.type;
-        if(type === Button){
-          return React.cloneElement(children, {icon: authButton.icon});
-        } else if(type === Popconfirm && children.props.children && children.props.children.type === Button){
-          let newChildren =  React.cloneElement(children.props.children , {icon: authButton.icon});
-          return React.cloneElement(children, {children: newChildren});
-        } else{
+        if (type === Button) {
+          return React.cloneElement(children, { icon: authButton.icon });
+        } else if (
+          type === Popconfirm &&
+          children.props.children &&
+          children.props.children.type === Button
+        ) {
+          let newChildren = React.cloneElement(children.props.children, {
+            icon: authButton.icon,
+          });
+          return React.cloneElement(children, { children: newChildren });
+        } else {
           return children;
         }
       } else {
         return children;
       }
     } else {
-      return null;
+      return noAuth || null;
     }
   }
 }
 
 AuthButton.propTypes = {
   perms: PropTypes.string.isRequired,
-  children: PropTypes.element.isRequired
-}
+  noAuth: PropTypes.element,
+  children: PropTypes.element.isRequired,
+};
+
+AuthButton.defaultProps = {
+  noAuth: null,
+};
