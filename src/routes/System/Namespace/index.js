eb468110d226d9efc32c00a34403e1fb6f85ca2b
‘xcsnx’ <1192709687@qq.com>
2024-07-14 14:41:32 +0800

    fix

--- src/routes/System/Namespace/index.js
+++ src/routes/System/Namespace/index.js
@@ -272,7 +272,7 @@ export default class Namespace extends Component {
           fixed: "right",
           render: (text, record) => {
             return record.namespaceId ===
-            "649330b6-c2d7-4edc-be8e-8a54df9eb385" ? (
+              "649330b6-c2d7-4edc-be8e-8a54df9eb385" ? (
               ""
             ) : (
               <div className="optionParts">
