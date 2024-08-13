9d8d42756c73abb1d71d8cb08d1faa64b11c5801
‘xcsnx’ <1192709687@qq.com>
2024-08-13 12:48:40 +0800

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -417,6 +417,7 @@ export async function findAuth(params) {
 
 /* add selector */
 export async function addSelector(params) {
+  params.namespaceId='649330b6-c2d7-4edc-be8e-8a54df9eb385'
   return request(`${baseUrl}/selector`, {
     method: `POST`,
     body: {
@@ -427,14 +428,19 @@ export async function addSelector(params) {
 
 /* delete selector */
 export async function deleteSelector(params) {
+  params.namespaceId='649330b6-c2d7-4edc-be8e-8a54df9eb385'
   return request(`${baseUrl}/selector/batch`, {
     method: `DELETE`,
-    body: [...params.list],
+    body: {
+      ids: [...params.list],
+      namespaceId: params.namespaceId,
+    },
   });
 }
 
 /* update selector */
 export async function updateSelector(params) {
+  params.namespaceId='649330b6-c2d7-4edc-be8e-8a54df9eb385'
   return request(`${baseUrl}/selector/${params.id}`, {
     method: `PUT`,
     body: {
@@ -450,12 +456,14 @@ export async function enableSelector(params) {
     body: {
       ids: params.list,
       enabled: params.enabled,
+      namespaceId: '649330b6-c2d7-4edc-be8e-8a54df9eb385'
     },
   });
 }
 
 /* get all selectors */
 export async function getAllSelectors(params) {
+  params.namespaceId="649330b6-c2d7-4edc-be8e-8a54df9eb385"
   return request(`${baseUrl}/selector?${stringify(params)}`, {
     method: `GET`,
   });
@@ -463,7 +471,7 @@ export async function getAllSelectors(params) {
 
 /* get single selector */
 export async function findSelector(params) {
-  return request(`${baseUrl}/selector/${params.id}`, {
+  return request(`${baseUrl}/selector/${params.id}/649330b6-c2d7-4edc-be8e-8a54df9eb385`, {
     method: `GET`,
   });
 }
